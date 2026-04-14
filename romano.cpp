8054c49f2b0eeb29cccf954f05a5a81a67b16ac0
davilb64 <davilopesbrito64@gmail.com>
2026-04-13 23:32:46 -0300

    Green 1 - Converte char diferente de I

--- romano.cpp
+++ romano.cpp
@@ -6,6 +6,24 @@ int converteRomano(std::string numeroRomano) {
         if (c == 'I') {
             numeroDecimal++;
         }
+        if (c == 'V') {
+            numeroDecimal+=5;
+        }
+        if (c == 'X') {
+            numeroDecimal+=10;
+        }
+        if (c == 'L') {
+            numeroDecimal+=50;
+        }
+        if (c == 'C') {
+            numeroDecimal+=100;
+        }
+        if (c == 'D') {
+            numeroDecimal+=500;
+        }
+        if (c == 'M') {
+            numeroDecimal+=1000;
+        }
     }
     return numeroDecimal;
 }
