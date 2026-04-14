fe3cb310c77c056af8fbfed58779d48fb52f0942
davilb64 <davilopesbrito64@gmail.com>
2026-04-13 23:37:35 -0300

    Refatora - Switch case para reduzir ifs

--- romano.cpp
+++ romano.cpp
@@ -3,26 +3,30 @@
 int converteRomano(std::string numeroRomano) {
     int numeroDecimal = 0;
     for (const char c: numeroRomano) {
-        if (c == 'I') {
-            numeroDecimal++;
-        }
-        if (c == 'V') {
-            numeroDecimal+=5;
-        }
-        if (c == 'X') {
-            numeroDecimal+=10;
-        }
-        if (c == 'L') {
-            numeroDecimal+=50;
-        }
-        if (c == 'C') {
-            numeroDecimal+=100;
-        }
-        if (c == 'D') {
-            numeroDecimal+=500;
-        }
-        if (c == 'M') {
-            numeroDecimal+=1000;
+        switch (c) {
+            case 'I':
+                numeroDecimal++;
+                break;
+            case 'V':
+                numeroDecimal+=5;
+                break;
+            case 'X':
+                numeroDecimal+=10;
+                break;
+            case 'L':
+                numeroDecimal+=50;
+                break;
+            case 'C':
+                numeroDecimal+=100;
+                break;
+            case 'D':
+                numeroDecimal+=500;
+                break;
+            case 'M':
+                numeroDecimal+=1000;
+                break;
+            default:
+                return -1;
         }
     }
     return numeroDecimal;
