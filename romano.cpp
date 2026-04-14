1eeb902fae5fde79fd55c2ef08c0aa6ba8fbdc09
davilb64 <davilopesbrito64@gmail.com>
2026-04-14 13:44:11 -0300

    Green 1 - ConverteSubs

--- romano.cpp
+++ romano.cpp
@@ -1,34 +1,43 @@
 #include "romano.hpp"
+#include <vector>
 
 int converteRomano(std::string numeroRomano) {
     int numeroDecimal = 0;
+    std::vector<int> vetorConvertido;
     for (const char c: numeroRomano) {
         switch (c) {
             case 'I':
-                numeroDecimal++;
+                vetorConvertido.push_back(1);
                 break;
             case 'V':
-                numeroDecimal+=5;
+                vetorConvertido.push_back(5);
                 break;
             case 'X':
-                numeroDecimal+=10;
+                vetorConvertido.push_back(10);
                 break;
             case 'L':
-                numeroDecimal+=50;
+                vetorConvertido.push_back(50);
                 break;
             case 'C':
-                numeroDecimal+=100;
+                vetorConvertido.push_back(100);
                 break;
             case 'D':
-                numeroDecimal+=500;
+                vetorConvertido.push_back(500);
                 break;
             case 'M':
-                numeroDecimal+=1000;
+                vetorConvertido.push_back(1000);
                 break;
             default:
                 return -1;
         }
     }
+
+    for (size_t i = 0; i < vetorConvertido.size(); i++) {
+        if (i + 1 < vetorConvertido.size() && vetorConvertido[i] < vetorConvertido[i + 1]) {
+            numeroDecimal -= vetorConvertido[i];
+        } else {
+            numeroDecimal += vetorConvertido[i];
+        }
+    }
     return numeroDecimal;
 }
-
