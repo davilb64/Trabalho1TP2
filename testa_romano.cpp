888587278b4897bdc2b5be11879a099bf6c98340
davilb64 <davilopesbrito64@gmail.com>
2026-04-13 23:30:22 -0300

    Red 1 - Converte demais char fora o I

--- testa_romano.cpp
+++ testa_romano.cpp
@@ -4,7 +4,16 @@
         EXPECT_EQ(converteRomano("I"),1);
     }
 
-    TEST(RomanoTest, Convet2Char) {
+    TEST(RomanoTest, Convert2Char) {
         EXPECT_EQ(converteRomano("II"),2);
+    }
+
+    TEST(RomanoTest, ConvertCharDiferentesDeI) {
+        EXPECT_EQ(converteRomano("V"),5);
+        EXPECT_EQ(converteRomano("X"),10);
+        EXPECT_EQ(converteRomano("L"),50);
+        EXPECT_EQ(converteRomano("C"),100);
+        EXPECT_EQ(converteRomano("D"),500);
+        EXPECT_EQ(converteRomano("M"),1000);
     }
 
